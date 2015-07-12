fec4a59075a2e1bd45f401dcb9d767e6207f8617
Stuart Thompson <stuart.thompson@dowjones.com>
2015-07-12 18:43:30 -0400

    Init

---
+++ README.md
@@ -0,0 +1,34 @@
+# node-archieml-boilerplate
+A boilerplate for connecting to Google Drive, getting the file contents, then spitting it out with archieML.
+
+## Installation
+`npm install`
+
+## Setup
+
+### Enable the Drive API
+Use [this wizard](https://console.developers.google.com/start/api?id=drive) to create or select a project in the Google Developers Console and automatically enable the API.
+1. In the sidebar on the left, select Consent screen. Select an EMAIL ADDRESS, enter a PRODUCT NAME if not already set, and click the Save button.
+2. In the sidebar on the left, select APIs & auth and then Credentials. In the new tab that opens, click Create new Client ID.
+3. Select the application type Installed application, the installed application type Other, and click the Create Client ID button.
+4. Click the Download JSON button under your new client ID. Move this file to your working directory and rename it client_secret.json.
+
+### Authorize the app
+You must run `node index.js` once to authorize the app. After that, you don't need to authorize again because the information is stored in the file system.
+
+1. `node index.js`
+2. Browse to the provided URL in your web browser. If you are not already logged into your Google account, you will be prompted to log in. If you are logged into multiple Google accounts, you will be asked to select one account to use for the authorization.
+3. Click the Accept button.
+4. Copy the code you're given, paste it into the command-line prompt, and press Enter.
+
+### Run the app
+You can choose to run the app from a web server using Express or from the terminal itself.
+
+#### From express
+1. `node express.js`
+2. Go to `localhost:3000/YOUR_DOCUMENT_KEY_HERE`
+
+#### From Terminal
+1. Edit the file `index.js` to include your key in the variable `fileId`
+2. Open Terminal
+3. `node index.js`
---
+++ express.js
@@ -0,0 +1,148 @@
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var archieml = require('archieml');
+var express = require('express');
+var app = express();
+
+app.get('/:key',function(req,res){
+  // Edit the file ID for the Google Doc. Be sure it's set to the "Share" preferences to "anyone on the internet"
+  var fileId = '1EJdvZQmQahxB6WHD2-Pe_0_SCPMV1DEf1fWXbD-deQE';
+
+  // Overall varibales
+  var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+  var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+      process.env.USERPROFILE) + '/.credentials/';
+  var TOKEN_PATH = TOKEN_DIR + 'drive-api-quickstart.json';
+
+  // Load client secrets from a local file.
+  var oauth2Client;
+  fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+    if (err) {
+      console.log('Error loading client secret file: ' + err);
+      return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Drive API.
+    authorize(JSON.parse(content), getExportLink);
+  });
+
+  /**
+   * Create an OAuth2 client with the given credentials, and then execute the
+   * given callback function.
+   *
+   * @param {Object} credentials The authorization client credentials.
+   * @param {function} callback The callback to call with the authorized client.
+   */
+  function authorize(credentials, callback) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var auth = new googleAuth();
+    oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function(err, token) {
+      if (err) {
+        getNewToken(oauth2Client, callback);
+      } else {
+        oauth2Client.credentials = JSON.parse(token);
+        callback(oauth2Client);
+      }
+    });
+  }
+
+  /**
+   * Get and store new token after prompting for user authorization, and then
+   * execute the given callback with the authorized OAuth2 client.
+   *
+   * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+   * @param {getEventsCallback} callback The callback to call with the authorized
+   *     client.
+   */
+  function getNewToken(oauth2Client, callback) {
+    var authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function(code) {
+      rl.close();
+      oauth2Client.getToken(code, function(err, token) {
+        if (err) {
+          console.log('Error while trying to retrieve access token', err);
+          return;
+        }
+        oauth2Client.credentials = token;
+        storeToken(token);
+        callback(oauth2Client);
+      });
+    });
+  }
+
+  /**
+   * Store token to disk be used in later program executions.
+   *
+   * @param {Object} token The token to store to disk.
+   */
+  function storeToken(token) {
+    try {
+      fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+      if (err.code != 'EEXIST') {
+        throw err;
+      }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
+  }
+
+  function getFileContents(exportLink){
+    oauth2Client.request({
+        method: 'GET',
+        uri: exportLink
+    }, function(err, body) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        // remove Google Docs comments
+        body = body.replace(/\[[a-z]{1,2}\]/g,'');
+
+        console.log(body);
+        var data = archieml.load(body);
+
+        console.log(data);
+        res.json(data);
+    });
+  }
+
+  function getExportLink(auth){
+    var service = google.drive('v2');
+    var request = service.files.get({
+      auth: auth,
+      fileId: fileId
+    },function(err,response){
+      if(err){
+        console.log(err);
+        return;
+      }
+
+      var exportLink = response['exportLinks']['text/plain'];
+
+      getFileContents(exportLink);
+
+    });
+  }
+});
+
+var server = app.listen(3000,function(){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Server running at %s:%s', host, port);
+});
